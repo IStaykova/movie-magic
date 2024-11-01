40431cd7bd46a8a47c8a48b360e13f4d952c7099
IStaykova <164316912+IStaykova@users.noreply.github.com>
2024-11-01 20:48:01 +0200

    add Search page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,6 +4,20 @@ exports.getAll = () => {
     return movies.slice();
 };
 
+exports.search = (title, genre, year) => {
+    let result = movies.slice();
+    if(title){
+        result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
+    }
+    if(genre){
+        result = result.filter(movie => movie.genre === genre);
+    }
+    if(year){
+        result = result.filter(movie => movie.year === year);
+    }
+    return result;
+}
+
 exports.getOne = (movieId) => {
     const movie = movies.find(movie => movie._id == movieId);
     return movie;
