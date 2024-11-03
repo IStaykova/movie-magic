12183961e257897221e6a9cd3d97fa30084b3a7a
IStaykova <164316912+IStaykova@users.noreply.github.com>
2024-11-03 12:41:02 +0200

    add POST for cast/create

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -3,5 +3,10 @@ const router = require('express').Router();
 router.get('/cast/create', (req, res) => {
     res.render('cast/create');
 });
+router.post('/cast/create', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    res.redirect('/');
+})
 
 module.exports = router;
