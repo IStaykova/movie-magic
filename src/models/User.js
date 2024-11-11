3793aadc61dad78810b3d3e988c28bc683fed702
IStaykova <164316912+IStaykova@users.noreply.github.com>
2024-11-11 20:32:40 +0200

    add login page

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+
+    }
+});
+
+userSchema.pre('save', async function(){
+   
+    const hash = await bcrypt.hash(this.password, 12);
+
+    this.password = hash;
+});
+
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
