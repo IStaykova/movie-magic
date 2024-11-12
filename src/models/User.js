494973868ca065a1fa713f31e56fd0426b1ad352
IStaykova <164316912+IStaykova@users.noreply.github.com>
2024-11-12 09:17:49 +0200

    Generate jwt on successful login

--- src/models/User.js
+++ src/models/User.js
@@ -6,22 +6,29 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        lowercase: true,
+        unique: true,
     },
     password: {
         type: String,
         required: true,
 
-    }
+    },
 });
 
 userSchema.pre('save', async function(){
    
     const hash = await bcrypt.hash(this.password, 12);
-
     this.password = hash;
 });
 
-
+userSchema.virtual('rePassword')
+ .set(function(value){
+    if(value !== this.password){
+        throw new mongoose.Error('Password mismatch');
+    }
+   
+ });
 
 const User = mongoose.model('User', userSchema);
 
